dfa124d04120c7dc37354a77ad142a38ce5b10d1
Юрий Девляшов <YYDEVLYASHOV@MG160.mg.loc>
2020-09-23 18:21:08 +0300

    server ok

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <vChat/>
+  </div>
+</template>
+
+<script>
+import VueSocketIO from 'vue-socket.io'
+
+import vChat from './components/chat/Chat.vue'
+// import foo from "foo"
+
+
+
+export default {
+  name: 'App',
+  components: {
+    vChat
+  }
+}
+</script>
+
+<style>
+
+</style>
