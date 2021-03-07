284d4dd73003f6580babf4cfec75595bc04babf1
Юрий Девляшов <YYDEVLYASHOV@BG.loc>
2021-03-08 01:39:40 +0300

    start

--- src/server/server.js
+++ src/server/server.js
@@ -1,35 +1,33 @@
-console.log('Сервер запущен!!!')
-let express = require('express')
-let app = express()
-let server = require('http').createServer(app)
-let io = require('socket.io').listen(server)
+console.log("Сервер запущен!!!");
+let express = require("express");
+let app = express();
+let server = require("http").createServer(app);
+let io = require("socket.io").listen(server);
 
-server.listen(8080)
+server.listen(8080);
 
 // отслеживаемый адрес
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 // Пользователи
-users = []
+users = [];
 // Все подключения
-allConnections = []
+allConnections = [];
 
-io.sockets.on('connection', (socket) => {
-    // добавить подключение
-    console.log('Соединение установлено!')
-    allConnections.push(socket)
+io.sockets.on("connection", socket => {
+  // добавить подключение
+  console.log("Соединение установлено!");
+  allConnections.push(socket);
 
-    socket.on('disconnect', ()=> {
-        allConnections.splice(allConnections.indexOf(socket), 1)
-        console.log('Отключено!')
-    })
+  socket.on("disconnect", () => {
+    allConnections.splice(allConnections.indexOf(socket), 1);
+    console.log("Отключено!");
+  });
 
-    // получаем сообщение
-    socket.on('send Message', (data) => {
-        io.sockets.emit('add message', {message: data.message, name: data.name})
-    })
-
-
-})
+  // получаем сообщение
+  socket.on("send Message", data => {
+    io.sockets.emit("add message", { message: data.message, name: data.name });
+  });
+});
